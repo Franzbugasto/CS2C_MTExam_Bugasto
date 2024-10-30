3ae97e5f36185804813a2e916343b7fe69d4d77d
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-30 18:07:23 +0800

    userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,24 @@
+// Collect the user's age
+let age = 21; // Replace this with the actual age
+// Categorize based on age
+if (age < 5) {
+    console.log("Toddler (under 5 years)");
+} else {
+    if (age <= 12) {
+        console.log("Child (5-12 years)");
+    } else {
+        if (age <= 19) {
+            console.log("Teenager (13-19 years)");
+        } else {
+            if (age <= 35) {
+                console.log("Young Adult (20-35 years)");
+            } else {
+                if (age <= 60) {
+                    console.log("Middle-Aged (36-60 years)");
+                } else {
+                    console.log("Senior (over 60 years)");
+                }
+            }
+        }
+    }
+}
