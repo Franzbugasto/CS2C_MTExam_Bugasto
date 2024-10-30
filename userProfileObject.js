af4cb002185da7d2ec9f44caa95429103b6078d4
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-30 18:01:00 +0800

    userProfileObject.js

---
+++ userProfileObject.js
@@ -0,0 +1,9 @@
+// Create an object to store user information
+let userProfile = {
+    fullName: "Franz Gann Marc V.Bugasto",
+    age: 21,
+    favoriteNumber: 95,
+    favoriteColor: "Red"
+};
+// Log the entire object to the console
+console.log(userProfile);
