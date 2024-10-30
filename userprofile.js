1389d9ae6acae89c4fa6a572f9c1cea8b80aecac
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-30 18:05:30 +0800

    userprofile.js

--- userprofile.js
+++ userprofile.js
@@ -1,8 +1,9 @@
+// Declare variables and assign values
 let fullName = "Franz Gann Marc V.Bugasto";
 let age = 21;
 let favoriteNumber = 95;
 let favoriteColor = "Red";
-
+// Log the information to the console
 console.log("Full Name:",fullName);
 console.log("Age:",age);
 console.log("Favorite Number:",favoriteNumber);
