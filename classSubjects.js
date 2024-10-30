d8314512b2139596cd921b8d86dd84ad5856b52a
Franz Gann Marc V. Bugasto <franzgannmarcvbugasto@gmail.com>
2024-10-30 17:56:41 +0800

    classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,8 @@
+// Declare variables and assign values
+let subjectTitle = "Data Structure's and algorithm";
+let classSchedule = "Monday, 8:00 AM - 9:00 AM";
+let classroom = "Comlab B";
+let classInstructor = "Professor Pascua";
+let studentName = "Franz Bugasto";
+// Log the information in the specified format
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
